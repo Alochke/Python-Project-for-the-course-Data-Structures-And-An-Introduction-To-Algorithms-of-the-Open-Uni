ed29820a7bf513f3b241d44e21df09052aacc300
sageuthanasia <sageuthanasia@gmail.com>
2022-01-03 04:04:08 +0200

    Added comments.

--- Node.py
+++ Node.py
@@ -1,18 +1,23 @@
 # The next class represents a Node of a linked-list, used primarily in the Linked-List class.
 
 class Node:
+    # A default-ive constructor for a Node object, setting up its value (val) and its following Node (next).
     def __init__(self, to_be_val, to_be_next):
         self.next = to_be_next
         self.val = to_be_val
 
+    # A setter function for self's next node.
     def set_next(self, to_be_next):
         self.next = to_be_next
 
+    # A setter function for self's value.
     def set_val(self, to_be_val):
         self.val = to_be_val
 
+    # A getter function for self's next node.
     def get_next(self):
         return self.next
 
+    # A getter function for self's value
     def get_val(self):
         return self.val
