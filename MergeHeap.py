23d1d97853902926918c0324d42a1cdfb538a3d3
sageuthanasia <sageuthanasia@gmail.com>
2022-01-03 07:38:57 +0200

    Added a __main__ and MergeHeap modules. Currently and lastly working on finishing main()'s structure, help is welcomed but keep in mind that it is required that the main function will be constructed out of function calls only.

---
+++ MergeHeap.py
@@ -0,0 +1,32 @@
+import LinkedList
+import __main__
+
+
+# A class representing a min-heap represented by a linked-list (utilizing the LinkedList class.).
+
+class MergeHeap(LinkedList.LinkedList):
+    # The variable below determines in which form out of three the terminal of the main function expects the
+    # data to be given as input:
+    # The value 1 is for sorted lists.
+    # The value 2 is for un-sorted lists.
+    # The value 3 is for un-sorted, disjointed lists.
+    mode = None
+
+    # A default-ive constructor, setting the head to to_be_head.
+    def __init__(self, to_be_head):
+        super().__init__(to_be_head)
+
+    # A setter function for mode.
+    @classmethod
+    def set_mode(cls, to_be_mode):
+        cls.mode = to_be_mode
+
+    # A function which turns given linked-lists into mergeable-min-heaps, and the way it does so relies on the
+    # value of cls.mode,
+    # The function relies on it to treat the list given as parameter in maximal efficiency.
+    @classmethod
+    def MakeHeap(cls, list):
+        if cls.mode == 1:
+            return cls(list.getHead())
+        else:
+            pass
---
+++ __main__.py
@@ -0,0 +1,27 @@
+import MergeHeap
+import os
+
+
+# This module is the main function of the program, which acts as a command terminal. For controlling,
+# changing, printing and creating mergeable heaps by command or by referring to the location of a txt-file of
+# commands.
+
+# Definition of the main function to make use of recursion.
+def main():
+    print("Please decide in which form are you to input the data \n 1 - for sorted lists \n 2 - for un-sorted lists "
+          "\n 3 - for un-sorted, disjointed lists.")
+    MergeHeap.MergeHeap.set_mode(input())
+    os.system('cls')
+    print("...")
+
+
+# A function which clears the console.
+def clear_console():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+
+# The command below initiates the program.
+main()
