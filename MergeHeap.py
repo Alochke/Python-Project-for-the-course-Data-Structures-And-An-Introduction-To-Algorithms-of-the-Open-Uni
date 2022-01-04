2795fef31f3d643da6aaba21e8462f633ad2cddc
sageuthanasia <sageuthanasia@gmail.com>
2022-01-04 09:24:11 +0200

    - Changed main() to execute(command, *args), just to be free of the expectation of it being composed of function calls only, therefore we will refer to the script/body of __main__.py as the main function, for more info on the new parameters check the docString. - Changed the lines coming after the if statements in line 57 in __main__.py, for correct chronological order of the recursive printing before calls of execute and added a call to init() to make the use of print_pos() possible. - Added to _main_.py the function print_pos. - Got rid of the __init__ function in MergeHeap.py because it did not do anything that the constructor of super doesn't do, also got rid of the MakeHeap function in the module because the booklet defines it as a constructor of an empty Heap so anyway there is no need for it to be.

--- MergeHeap.py
+++ MergeHeap.py
@@ -16,25 +16,7 @@ class MergeHeap(LinkedList.LinkedList):
     """
     mode = None
 
-    def __init__(self, to_be_head):
-        """Construct a MergeHeap and set up the self.head to to_be_head."""
-        super().__init__(to_be_head)
-
     @classmethod
     def set_mode(cls, to_be_mode):
         """Set cls.mode to to_be_mode."""
         cls.mode = to_be_mode
-
-    @classmethod
-    # The function below Doesn't follow function-naming conventions to suite itself to the way __main__.main()'s
-    # terminal commands are to be given as input.
-    def MakeHeap(cls, list):
-        """
-        Turn list into mergeable-min-heap.
-        """
-
-        # The function relies on the value of cls.mode to determine the way it works to maximize efficiency.
-        if cls.mode == 1:
-            return cls(list.getHead())
-        else:
-            pass
--- __main__.py
+++ __main__.py
@@ -8,26 +8,58 @@ class.
 
 import MergeHeap
 import os
+import sys
+from colorama import *
 
 
-def main():
-    """Execute the terminal."""
-    print("Please decide in which form are you to input the data \n 1 - for sorted lists \n 2 - for un-sorted lists "
-          "\n 3 - for un-sorted, disjointed lists.")
-    MergeHeap.MergeHeap.set_mode(input())
+def execute(command, *args):
+    """
+    Execute the command given as parameter, while using a tuple (args) that contains all the created MergeHeaps,
+    in chronological order from left to right.
+    """
     clear_console()
+
+    if command == "FromTxt":
+        pass
+
+    if command == "MakeHeapFromList":
+        pass
+
     print("Pleas chose one of the following commands: \n"
-          "MakeHeap, MakeHeapFromList, Insert [int], ExtractMin, Minimum, ExtractMin, Union, FromTxt")
+          "MakeHeap, MakeHeapFromList, Insert [int], ExtractMin, Minimum, Union, FromTxt")
+
+    if command == "MakeHeap":
+        execute(input(), *args, MergeHeap.MergeHeap(None))
+    if command[0] == "I":
+        pass
+    if command == "ExtractMin":
+        pass
+    if command == "Minimum":
+        pass
+    # If none of the if statements above were entered then command == "Union".
+    pass
 
 
 def clear_console():
-    """A function which clears the console."""
+    """Clear the console."""
     command = 'clear'
     if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
         command = 'cls'
     os.system(command)
 
 
-# The command below initiates the program.
+def print_pos(x, y, text_to_print):
+    """Prints to (x,y) position the text given as parameter to cmd. Maybe useful in the future."""
+    sys.stdout.write("\x1b[%d;%df%s" % (x, y, text_to_print))
+    sys.stdout.flush()
+
+
 if __name__ == "__main__":
-    main()
+    init()
+    print("Please decide in which form are you to input the data \n 1 - for sorted lists \n 2 - for un-sorted lists "
+          "\n 3 - for un-sorted, disjointed lists.")
+    MergeHeap.MergeHeap.set_mode(input())
+    clear_console()
+    print("Pleas chose one of the following commands: \n"
+          "MakeHeap, MakeHeapFromList, Insert [int], ExtractMin, Minimum, Union, FromTxt")
+    execute(input())
